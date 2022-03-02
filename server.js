c1bc9405c0933b33d60bc7c086983df3d4a81308
becket <becket.bowes@gmail.com>
2022-03-01 22:03:59 -0500

    returning database

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 //default response for any other req
 app.use((req, res) => {
     res.status(404).end();
