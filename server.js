87e2fea4d8942ddc50822b977955f3aa93cbe27f
becket <becket.bowes@gmail.com>
2022-03-01 21:59:36 -0500

    server running!

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'sql121!!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+//default response for any other req
+app.use((req, res) => {
+    res.status(404).end();
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
