c06d9e7d6dfd0a984c1e32773d25956bd313ce11
becket <becket.bowes@gmail.com>
2022-03-03 00:07:50 -0500

    lesson update

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'sql121!!',
-    database: 'election'
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'sql121!!',
+  database: 'election'
 });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const mysql = require("mysql2");
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
-const PORT = process.env.PORT || 3011;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-// Not Found response for unmatched routes
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
